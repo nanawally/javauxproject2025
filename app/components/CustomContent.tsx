4108d7e2c99ba52ac59c055aeb6a0794b851955d
Mojujo <oscar.tornqvist01@gmail.com>
2025-03-19 11:04:31 +0100

    trying fetch features

--- app/components/CustomContent.tsx
+++ app/components/CustomContent.tsx
@@ -1,24 +1,26 @@
 import styles from "./CustomContent.module.css"
 
 export function CustomContent() {
-    return (
-        <section className={styles.box}>
-        <ul className={styles.list}>
-          <li>
-            <img src="app/pictures/strawberry-smoothie.jpg" alt="" className={styles.picture}/>
-            <p>Template</p>
-          </li>
-          <li>
-            <article>
-              <p>Template</p>
-            </article>
-          </li>
-          <li>
-            <article>
-              <p>Template</p>
-            </article>
-          </li>
-        </ul>
-      </section> 
-    )
+  return (
+    <section className={styles.box}>
+      <ul className={styles.list}>
+        <li>
+          <img src="app/pictures/strawberry-smoothie.jpg" alt="" className={styles.picture} />
+          <p>Template</p>
+        </li>
+        <li>
+          <img src="app/pictures/strawberry-smoothie.jpg" alt="" className={styles.picture} />
+          <p>Template</p>
+        </li>
+        <li>
+          <img src="app/pictures/strawberry-smoothie.jpg" alt="" className={styles.picture} />
+          <p>Template</p>
+        </li>
+        <li>
+          <img src="app/pictures/strawberry-smoothie.jpg" alt="" className={styles.picture} />
+          <p>Template</p>
+        </li>
+      </ul>
+    </section>
+  )
 }
