c2b239d932d582f4d0e4f7adac77f286b3eb81e9
Mojujo <oscar.tornqvist01@gmail.com>
2025-03-20 23:26:50 +0100

    revisit recipe site final update added more font options and extended footer functionality

--- app/pages/recipe/recipe.tsx
+++ app/pages/recipe/recipe.tsx
@@ -1,41 +1,45 @@
 import { Link } from "react-router";
 import styles from "./recipe.module.css";
 import { CustomHeader } from "~/components/CustomHeader";
+import { CustomFooter } from "~/components/CustomFooter";
 
 export function Recipe() {
   return (
     <>
-      <CustomHeader />
-      <main className={styles.recipeTheme}>
-        <div className={styles.center}>
-          <h1 className={styles.title}>Banansmoothie</h1>
-        </div>
-        <div className={styles.center}>
-          <section>
-            <div className={styles.posts}>
-              <article className={styles.individualpost}>
-                <img src="/pictures/banansmoothie.jpg" alt="bananaSmoothie" />
-                <h3>Ingredienser</h3>
+      <div className={styles.pageWrapper}>
+        <CustomHeader />
+        <main className={styles.recipeTheme}>
+          <div className={styles.center}>
+            <h2 className={styles.title}>Banansmoothie</h2>
+          </div>
+          <div className={styles.center}>
+            <section>
+              <div className={styles.posts}>
+                <article className={styles.individualpost}>
+                  <img src="/assets/smoothies/Banansmoothie.jpg" alt="bananaSmoothie" />
+                  <h3>Ingredienser</h3>
 
-                <ul>
-                  <li>1 banan</li>
-                  <li>3 dl havrebaserad dryck</li>
-                  <li>2 tsk brun farin</li>
-                  <li>1 krm mald kanel (kan uteslutas)</li>
-                </ul>
+                  <ul>
+                    <li>1 banan</li>
+                    <li>3 dl havrebaserad dryck</li>
+                    <li>2 tsk brun farin</li>
+                    <li>1 krm mald kanel (kan uteslutas)</li>
+                  </ul>
 
-              </article>
-              <article className={styles.individualpost}>
-                <h3>Gör så här</h3>
-                <p>
-                  Blanda alla ingredienser i en blender, matberedare eller mixa
-                  med en mixerstav.
-                </p>
-              </article>
-            </div>
-          </section>
-        </div>
-      </main>
+                </article>
+                <article className={styles.individualpost}>
+                  <h3>Gör så här</h3>
+                  <p>
+                    Blanda alla ingredienser i en blender, matberedare eller mixa
+                    med en mixerstav.
+                  </p>
+                </article>
+              </div>
+            </section>
+          </div>
+        </main>
+        <CustomFooter />
+      </div>
     </>
   );
 }
