d128f18153d8009f843576a777a728d0fa0df861
HenrikJavaYH <henrik_champion@hotmail.com>
2025-03-20 19:18:40 +0100

    Changed headline to Gr8 smoothies

--- app/pages/home/home.tsx
+++ app/pages/home/home.tsx
@@ -4,7 +4,7 @@ import styles from "./home1.module.css";
 export function Home() {
   return (
     <main className={styles.heroImage}>
-      <h1>Välkommen till smoothie-himlen!</h1>
+      <h1>Välkommen till Gr8 smoothies!</h1>
       <p>Utforska vår hemsida för att hitta goda smoothie-recept!</p>
       <div className={styles.content}>
         <Link to="../about" className={styles.button}> Hitta smoothies</Link>
