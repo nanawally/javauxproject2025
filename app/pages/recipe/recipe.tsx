e212296b7e4277a3e49940641de0fd72179b9121
Mojujo <oscar.tornqvist01@gmail.com>
2025-03-25 16:05:39 +0100

    fixed css removed margin included padding

--- app/pages/recipe/recipe.tsx
+++ app/pages/recipe/recipe.tsx
@@ -1,11 +1,13 @@
 import { CustomContentRecipe } from "~/components/Recipes/CustomContentRecipe";
 import styles from "./recipe.module.css";
+import { CustomFooter } from "~/components/UI/CustomFooter";
 
 export function Recipe() {
   return (
     <>
       <main className={styles.recipeTheme}>
         <CustomContentRecipe />
+        <CustomFooter />
       </main>
     </>
   ); 
