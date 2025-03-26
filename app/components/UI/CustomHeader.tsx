80ac0f6ba6541eb77fba6f59378e1bb2b83c7457
Mojujo <oscar.tornqvist01@gmail.com>
2025-03-26 16:25:49 +0100

    started favorite page logic; going back to context and will merge favoritecontext and recipecontext

--- app/components/UI/CustomHeader.tsx
+++ app/components/UI/CustomHeader.tsx
@@ -5,7 +5,13 @@ export function CustomHeader() {
     return (
         <header className={styles.centerHeader}>
             <Link to="../" className={styles.link}>
+                <h4 className={styles.favoritesLink}>Placehold</h4>
+            </Link>
+            <Link to="../" className={styles.link}>
                 <img src="/assets/logo.jpg" alt="Logo" className={styles.logo} />
+            </Link>
+            <Link to="favorites" className={styles.link}>
+                <img src="/assets/Favorite-filled.jpg" alt="Dina favoriter" className={styles.favoritesLink} />
             </Link>
         </header>
     )
--- app/pages/favorites/favorites.tsx
+++ app/pages/favorites/favorites.tsx
@@ -1,6 +1,35 @@
+import { useFavorites } from "~/components/Favorites/FavoritesContext"
+import styles from "./favorites.module.css"
+import { useRecipeContext } from "~/components/Recipes/RecipeContext";
+import { Link } from "react-router";
+
 export function Favorites() {
+    const { favorites } = useFavorites();
+    const { setRecipeIndex } = useRecipeContext();
+
+    function handleRecipeClick(id: number) {
+        setRecipeIndex(id);
+    }
+
     return (
         <>
+            <main className={styles.favoriteTheme}>
+                <div className={styles.pageWrapper}>
+                    <h1>Dina favoriter</h1>
+                    {favorites.length === 0 ? (
+                        <div className={styles.empty}>
+                            <p>Du har inga sparade recept.</p>
+                        </div>
+                    ) : (
+                        <div className={styles.grid}>
+                            {favorites.map((recipeId) => (
+                                <Link to="recipe" className={styles.recipeLink} onClick={() => handleRecipeClick(recipeId)}>
+                                </Link>
+                            ))}
+                        </div>
+                    )}
+                </div>
+            </main>
         </>
     )
 }
