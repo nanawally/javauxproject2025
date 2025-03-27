86649185b0f3f0fd69a903ac96391d6dbdac76e2
Mojujo <oscar.tornqvist01@gmail.com>
2025-03-27 10:21:58 +0100

    added hamerbugermenu functionality; added hooks to allows for mouselisteners and dynamic closing

--- app/components/UI/CustomHeader.tsx
+++ app/components/UI/CustomHeader.tsx
@@ -1,19 +1,16 @@
 import { Link } from "react-router";
 import styles from "./CustomHeader.module.css"
-import { HamburgerMenu } from "./HamburgerMenu";
+import { HamburgerMenu } from "../Navigation/HamburgerMenu";
 
 export function CustomHeader() {
     return (
         <header className={styles.centerHeader}>
             <HamburgerMenu/>
             <Link to="../" className={styles.link}>
-                <h4 className={styles.favoritesLink}>Placehold</h4>
-            </Link>
-            <Link to="../" className={styles.link}>
                 <img src="/assets/logo.jpg" alt="Logo" className={styles.logo} />
             </Link>
             <Link to="favorites" className={styles.link}>
-                <img src="/assets/Favorite-filled.jpg" alt="Dina favoriter" className={styles.favoritesLink} />
+                <img src="/assets/Favorite-empty.jpg" alt="Dina favoriter" className={styles.favoritesLink} />
             </Link>
         </header>
     )
