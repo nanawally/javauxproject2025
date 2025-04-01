1fca05e65bd939759b64608eb3471935c7996735
maeka <maek.alekdawi@gmail.com>
2025-04-01 11:59:41 +0200

    Start Tabs component Anna,ELi

---
+++ app/components/Tabs/TabsContext.tsx
@@ -0,0 +1,28 @@
+import { Component } from "lucide-react";
+import { useContext, useState } from "react";
+
+export const TabsContext = React.createContext();
+
+export const withTabs = (Component) => {{children, ...props}} => {
+const [currentTab, setCurrentTab] = useState();
+
+return (
+    <TabsContext.Provider value={{currentTab, setCurrentTab}}>
+    <Component {...props}>
+        {children}
+    </Component> 
+     </TabsContext.Provider>
+);
+
+export const useTabs = () => {
+    const {currentTab, setCurrentTab} = useContext(TabsContext);
+    if(!TabsContext) {
+        throw new Error ('useTabs should be used inside TabsProvider ')
+
+    }
+    return {
+        currentTab, setCurrentTab
+    }
+}
+
+}
