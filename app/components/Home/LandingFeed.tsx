bb5073d2ed4e81e7decdd4c87ce0b204f9ccb5c0
Mojujo <oscar.tornqvist01@gmail.com>
2025-04-01 14:58:44 +0200

    basic media queries, starting on main content

--- app/components/Home/LandingFeed.tsx
+++ app/components/Home/LandingFeed.tsx
@@ -4,7 +4,10 @@ import styles from "./LandingFeed.module.css";
 export function LandingFeed() {
     return (
         <>
-        <HeroImage />
+            <HeroImage />
+            <section className={styles.shortDescription}>
+                <h2>Vi är Smoothify - här gör vi smoothies enkla, goda och inspirerande!</h2>
+            </section>
         </>
     );
 }
