7f8dc57d7e02ca37ef521e374bfd8dde5c39c4c5
Mojujo <oscar.tornqvist01@gmail.com>
2025-04-01 13:05:34 +0200

    made hero image responsive

--- app/components/Home/LandingFeed.tsx
+++ app/components/Home/LandingFeed.tsx
@@ -1,9 +1,10 @@
+import { HeroImage } from "./HeroImage";
 import styles from "./LandingFeed.module.css";
 
 export function LandingFeed() {
     return (
         <>
-        <h1>Landing page work in progress navigate to 'explore' for old landingpage</h1>
+        <HeroImage />
         </>
     );
 }
