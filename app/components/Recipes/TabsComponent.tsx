53f02146e1c73e0074ebfa2d7e31488692a6edde
Mojujo <oscar.tornqvist01@gmail.com>
2025-04-03 21:58:06 +0200

    first iteration of recipepage / tabs done awaiting more anna oscar parprogrammering

--- app/components/Recipes/TabsComponent.tsx
+++ app/components/Recipes/TabsComponent.tsx
@@ -18,13 +18,13 @@ export const TabsComponent = () => {
 
     const tabs: TabProps[] = [
         {
-            id: "ingredients", label: "Ingredienser", content: <ul className={styles.tabsIngredients}>{selectedRecipe?.ingredients.map((ingredient, index) => (
+            id: "ingredients", label: "Ingredienser", content: <ul className={styles.tabsList}>{selectedRecipe?.ingredients.map((ingredient, index) => (
                 <li key={index}>{ingredient}</li>
             ))} </ul>
         },
         { id: "instructions", label: "Gör så här", content: <div className={styles.tabsInstructions}>{selectedRecipe?.instructions}</div> },
         {
-            id: "nutrition", label: "Näringsvärde", content: <ul className={styles.tabsNutrients}>{selectedRecipe?.nutrition.map((nutrition, index) => (
+            id: "nutrition", label: "Näringsvärde", content: <ul className={styles.tabsList}>{selectedRecipe?.nutrition.map((nutrition, index) => (
                 <li key={index}>{nutrition}</li>
             ))}</ul>
         }
