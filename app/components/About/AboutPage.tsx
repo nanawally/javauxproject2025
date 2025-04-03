a789893c1a63e91d6f778949ac031e3039c63f1b
Anna Wallström <anoli89@hotmail.com>
2025-04-03 12:59:44 +0200

    About Us page design continued implementation

--- app/components/About/AboutPage.tsx
+++ app/components/About/AboutPage.tsx
@@ -4,13 +4,24 @@ export function AboutPage() {
   return (
     <>
       <main className={styles.aboutTheme}>
-        <div className={styles.textContainer}>
-          <h1>Hej smoothieälskare!</h1>
-          <p>
+        <div className={styles.bigTextContainer}>
+          <h1 className={styles.textContainers}>Hej smoothieälskare!</h1>
+          <p className={styles.textContainers}>
             Vi älskar smoothies! Men vi vet hur det är - lätt att fastna i samma
-            gamla recept eller tro att det krävs mer tid och ingredienser än det faktiskt gör.
+            gamla recept eller tro att det krävs mer tid och ingredienser än det
+            faktiskt gör.
           </p>
-          <p>Därför har vi samlat</p>
+          <p className={styles.textContainers}>
+            Därför har vi samlat de bästa och smidigaste recepten - för att göra
+            det enklare och roligare att mixa sig fram till nya favoriter!
+          </p>
+          <p className={styles.textContainers}>
+            Här hittar du snabba, goda och inspirerande recept. Oavsett om du
+            vill ha en hälsoboost, något barnvänligt eller en smoothie som
+            smakar dessert (ja, tack!). Så fram med mixern och låt oss göra
+            världen lite godare, en slurk i taget!
+          </p>
+          <h3 className={styles.textContainers}>-Smoothify Teamet</h3>
         </div>
       </main>
     </>
