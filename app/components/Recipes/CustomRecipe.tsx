a59440769c891849e85467b4d855e2b3fe954e10
Anna Wallström <anoli89@hotmail.com>
2025-04-08 15:31:01 +0200

    Recipe Page Desktop grid design implementation

--- app/components/Recipes/CustomRecipe.tsx
+++ app/components/Recipes/CustomRecipe.tsx
@@ -31,24 +31,64 @@ export function CustomRecipe() {
           <section className={styles.recipeContainer}>
             <div className={styles.recipeAspects}>
               <ul className={styles.recipeList}>
+                <h2>Ingredienser</h2>
                 {selectedRecipe?.ingredients.map((ingredient, index) => (
                   <li key={index}>{ingredient}</li>
                 ))}
               </ul>
               <div className={styles.recipeInstructions}>
+                <h2>Gör så här</h2>
                 {selectedRecipe?.instructions}
               </div>
               <ul className={styles.recipeList}>
+                <h2>Näringsvärde</h2>
                 {selectedRecipe?.nutrition.map((nutrition, index) => (
                   <li key={index}>{nutrition}</li>
                 ))}
               </ul>
+              <div className={styles.tipsContainerDesktop}>
+                <div className={styles.endCenter}>
+                  <h2>Allergier? Inga problem!</h2>
+                  <p>
+                    <img
+                      src="/assets/avocado.jpg"
+                      alt="Laktossymbol"
+                      className={styles.symbol}
+                    />
+                    (Laktos)
+                    <img
+                      src="/assets/avocado.jpg"
+                      alt="Glutensymbol"
+                      className={styles.symbol}
+                    />
+                    (Gluten)
+                    <img
+                      src="/assets/avocado.jpg"
+                      alt="Nöttersymbol"
+                      className={styles.symbol}
+                    />
+                    (Nötter)
+                  </p>
+                  <div className={styles.endCenter2}>
+                    <p>
+                      TIPS: Du kan enkelt göra våra smoothies allergivänliga.
+                      Byt bara ut allergenerna mot något likvärdigt som passar
+                      dig bättre!
+                    </p>
+                    <img
+                      src="/assets/avocado.jpg"
+                      alt=""
+                      className={styles.smiley}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
             <div className={styles.tabsContainer}>
               <TabsComponent />
             </div>
           </section>
-          <section className={styles.tipsContainer}>
+          <section className={styles.tipsContainerMobile}>
             <div className={styles.endCenter}>
               <h2>Allergier? Inga problem!</h2>
               <p>
@@ -71,13 +111,18 @@ export function CustomRecipe() {
                 />
                 (Nötter)
               </p>
-              <p>
-                TIPS: Du kan enkelt göra våra smoothies allergivänliga. Byt bara
-                ut allergenerna mot något likvärdigt som passar dig bättre!
-              </p>
-            </div>
-            <div className={styles.smiley}>
-              <img src="/assets/avocado.jpg" alt="" />
+              <div className={styles.endCenter2}>
+                <p>
+                  TIPS: Du kan enkelt göra våra smoothies allergivänliga. Byt
+                  bara ut allergenerna mot något likvärdigt som passar dig
+                  bättre!
+                </p>
+                <img
+                  src="/assets/avocado.jpg"
+                  alt=""
+                  className={styles.smiley}
+                />
+              </div>
             </div>
           </section>
         </div>
